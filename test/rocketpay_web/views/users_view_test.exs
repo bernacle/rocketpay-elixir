4971082b664e361f19d81abf5b96be1182335ee2
F1694326 Bruno Raphael Bifaroni Simplicio <brunoraphael@bb.com.br>
2021-02-27 14:09:29 -0300

    testing views

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,38 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase
+
+  import Phoenix.View
+
+  alias Rocketpay.{User, Account}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Bruno",
+      password: "123456",
+      nickname: "berna123",
+      email: "berna@gmail.com",
+      age: 31
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} =
+      Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message: "User created",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+        id: user_id,
+        name: "Bruno",
+        nickname: "berna123"
+      }
+    }
+
+    assert expected_response == response
+  end
+end
