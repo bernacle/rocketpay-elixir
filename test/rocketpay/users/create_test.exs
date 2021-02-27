49d326ceca95cf142db4f7decf52806d51d04dd8
F1694326 Bruno Raphael Bifaroni Simplicio <brunoraphael@bb.com.br>
2021-02-27 13:20:32 -0300

    testing users action create

---
+++ test/rocketpay/users/create_test.exs
@@ -0,0 +1,41 @@
+defmodule Rocketpay.Users.CreateTest do
+  use Rocketpay.DataCase
+
+  alias Rocketpay.User
+  alias Rocketpay.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, return an user" do
+      params = %{
+        name: "Bruno",
+        password: "123456",
+        nickname: "berna123",
+        email: "berna@gmail.com",
+        age: 31
+      }
+
+      {:ok, %User{id: user_id}} = Create.call(params)
+      user = Repo.get(User, user_id)
+
+      assert %User{name: "Bruno", age: 31, id: ^user_id} = user
+    end
+
+    test "when there are invalid params are valid, return an error" do
+      params = %{
+        name: "Bruno",
+        nickname: "berna123",
+        email: "berna@gmail.com",
+        age: 10
+      }
+
+      {:error, changeset} = Create.call(params)
+
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        password: ["can't be blank"]
+      }
+
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
